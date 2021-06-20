a9adf9039f4d033cf9d0f5d424986d6540017dec
rkhismet <rahim.hismet@gmail.com>
2021-06-20 23:53:40 +0600

    Last edits

--- src/main/java/com/telegrambot/WildberriesBot/WildberriesBotApplication.java
+++ src/main/java/com/telegrambot/WildberriesBot/WildberriesBotApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
-import org.telegram.telegrambots.ApiContextInitializer;
+//import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
